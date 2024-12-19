54c7e544e56f995634727c9aa8c03f9bc3cb3caa
Klaxxon43 <artax7878@gmail.com>
2024-12-19 21:42:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,15 @@ from app.heandlers import router
 from app.database.models import async_main
 from aiogram.types import Message
 
-token='7585776111:AAHwz-FOu9kZ96piHD0ZlU8QrUBxqNgS8Bc'
 
 async def main():
     await async_main()
-    bot = Bot(token='7585776111:AAHwz-FOu9kZ96piHD0ZlU8QrUBxqNgS8Bc')
+    bot = Bot(token='TOKEN')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
     
-bot = Bot(token='7585776111:AAHwz-FOu9kZ96piHD0ZlU8QrUBxqNgS8Bc')
+bot = Bot(token='TOKEN')
 
 if __name__ == '__main__':
     try:
